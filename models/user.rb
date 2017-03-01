be52f7a127d4216af628902dec0b89db1ba5064c
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-01 15:45:56 -0600

    Made User.all a class method and now DATABASE is constant in class

--- models/user.rb
+++ models/user.rb
@@ -1,43 +1,43 @@
 class User
+  DATABASE = Database.new
 
   # Create a user.
   # 
   # user_info - Hash of user info
   def create(user_info)
-    db = Database.new
     entry_string = "\"#{user_info["username"]}\","
     entry_string += "\"#{user_info["email"]}\",\"#{user_info["password"]}\""
-    db.newEntry("users", entry_string)
+    DATABASE.newEntry("users", entry_string)
   end
 
   # TODO Documentation
   def newPassword(currUser, newPass)
-  	db = Database.new
-  	hash = db.all("users", "username")
-  	hash[currUser]["password"] = newPass
-  	newLine = hash[currUser].values.join(",")
-  	db.edit("users", "username", currUser, newLine)
+  	all[currUser]["password"] = newPass
+  	newLine = all[currUser].values.join(",")
+  	DATABASE.edit("users", "username", currUser, newLine)
   end
 
   # TODO Documentation
   def getID(username)
-  	db = Database.new
-  	hash = db.all("users", "username")
-  	return hash[username]["userID"]
+  	return all.[username]["userID"]
   end
 
   # TODO Documentation
   def posts(username)
   	userID = getID(username)
-  	db = Database.new
   	results = []
-  	posts = db.all("posts", "timestamp")
+  	posts = DATABASE.all("posts", "timestamp")
   	posts.each do |k, v|
   		if v["userID"] == userID
   			results.push(k)
   		end
   	end
   	return results
+  end
+
+  # TODO Documentation
+  def User.all
+    DATABASE.all("users", "username")
   end
 
 end
