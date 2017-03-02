2a463d1a7ac8f9681f5577aa9b5a884cb4daeb0f
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-02 15:58:31 -0600

    BLAME GIT

--- main.rb
+++ main.rb
@@ -5,8 +5,8 @@ require 'sinatra'
 
 
 get("/"){
-	posts = Post.popular
-  @featured_post = Post.new(posts[0]).info
+	@posts = Post.popular
+  @featured_post = Post.new(@posts[0]).info
 
   # @posts_except_features_paginated = posts.page(params["page"])
   
