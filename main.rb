b2e01dda864dadc73948cb315289b94ab2e1a54e
Ben Nourse <bnourse@gmail.com>
2017-03-03 10:13:52 -0600

    prepping to integrate article.erb

--- main.rb
+++ main.rb
@@ -4,13 +4,25 @@ require_relative './models/user.rb'
 require_relative './services/score.rb'
 require 'sinatra'
 
+cur_page = 1
 
 get("/"){
     @featured = Post.featured("newest")
-    @page_of_posts = Post.page("newest",1)
+    @page_of_posts = Post.page("newest",cur_page)
   	erb :index
+}
+
+get("/next_page") {
+	cur_page += 1
+	redirect "/"	
+}
+
+get("/prev_page") {
+	cur_page -= 1
+	if cur_page < 1 then cur_page = 1 end
+	redirect "/"	
 }
 
 get("/newPost"){
 	erb :newPost
-}
+}	
