c14dea8bbb18d9da4e755ba1332da3ec6f3a4a53
Aaron Steward <aa.daly.stew@gmail.com>
2017-03-03 11:35:07 -0600

    copied models and services into spec files

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,49 @@
+class User
+  DATABASE = Database.new
+
+  def initialize(username)
+    @username = username
+    @info = DATABASE.find("users", "username", @username)
+  end
+
+  # Create a user.
+  # 
+  # user_info - Hash of user info
+  def User.create(user_info)
+    entry_string = "\"#{user_info["username"]}\",\"#{user_info["email"]}\",\"#{user_info["password"]}\""
+    DATABASE.newEntry("users", entry_string)
+  end
+
+  # Gathers user info and changes password value.
+  #
+  # newPass - String from user input
+  #
+  # Rebuilds user database.
+  def newPassword(newPass)
+    @info["password"] = newPass
+    DATABASE.edit("users", "username", @username, format_for_database)
+  end
+
+  # Format user's info for the database.
+  def format_for_database
+    @info.values.join(",")
+  end
+
+  # This user's posts.
+  def posts
+  	results = []
+  	posts = DATABASE.all("posts", "postID")
+  	posts.each do |k, v|
+  		if v["userID"] == @info["userID"]
+  			results.push(k)
+  		end
+  	end
+  	return results
+  end
+
+  # Gathers entire user database.
+  def User.all
+    DATABASE.all("users", "username")
+  end
+
+end
