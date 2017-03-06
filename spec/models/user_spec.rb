cc736c1bbb7dd5eff99b6d2efb0e1de8a3224ed6
Evin Grost <evin.grost@gmail.com>
2017-03-05 19:07:36 -0600

    Working on third user test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,63 @@
-RSpec.describe(User, ".all") do
-  it "creates a hash of all user data" do
+require "pry"
+# RSpec.describe(User, ".all") do
+#   it "creates a hash of all user data" do
 
-    expect(User.all).to be_a(Hash)
-    
+#     expect(User.all).to be_a(Hash)
+
+#   end
+# end
+
+# RSpec.describe("User", "format_for_database") do
+#   it "joins hash into comma separated string" do
+
+#   	#Setup
+#   	sampleUser = User.new("quixote")
+
+#   	actual = sampleUser.format_for_database
+
+#   	#Verify
+#   	#None required
+
+#   	#Exercise
+#     expect(actual).to include("quixote")
+
+#     #Teardown
+#     #None required
+#   end
+# end
+
+RSpec.describe("New user", "create") do
+  it "creates a new user with the given information and adds it to the database" do
+
+  	#Setup
+  	newUser = User.new("nennington")
+  	userInfoHash = {
+  		"userID" => "1488296341.172849",
+  		"username" => "nennington",
+  		"email" => "barlsworth@gmail.com",
+  		"password" => "iBark"
+  	}
+
+  	result = newUser.create
+
+  	#Verify
+  	#None required
+
+  	#Exercise
+    expect(result).to include("quixote")
+
+    #Teardown
+    #None required
   end
 end
+
+
+
+
+
+
+
+
+
+
+
