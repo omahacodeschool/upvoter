b9730477d27a87338e44d130188042dca1178a05
Aaron Steward <aa.daly.stew@gmail.com>
2017-03-06 14:23:38 -0600

    login functionality

--- main.rb
+++ main.rb
@@ -5,10 +5,14 @@ require_relative './models/post.rb'
 require_relative './models/user.rb'
 require_relative './services/score.rb'
 require 'sinatra'
+require 'pry'
+
+enable :sessions
 
 get("/") {
 	@cur_page = params["pg"].nil? ? 1 : params["pg"].to_i
 	@search   = params["search"].nil? ? "newest" : params["search"]
+	@user     = session[:user]
 		
     @page_of_posts = Post.page(@search, @cur_page)
 	erb :index
@@ -17,6 +21,33 @@ get("/") {
 get("/newPost") {
 	erb :newPost
 }
+
+get("/login") {
+	@err = session[:login] == "error"
+	session.clear
+	erb :login
+}
+
+post("/login") {
+	if User.loginValid?(params["username"],params["password"])
+		session[:login] = "true"
+		session[:user] = params["username"]
+		# redirect("/?search=" + @search + "&pg=" + @cur_page)
+		redirect("/")
+	else
+		session[:login] = "error"
+		redirect("/login")
+	end
+}
+
+post("/logout") {
+	session.clear
+	redirect("/")
+}
+
+# get("/login_error") {
+
+# }
 
 post("/newPost") {
 
