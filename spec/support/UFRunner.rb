d40f86a47da3c4f8f7e71bdc5caf6aa9858ead48
Ben Nourse <bnourse@gmail.com>
2017-03-06 11:58:28 -0600

    switching branches

--- spec/support/UFRunner.rb
+++ spec/support/UFRunner.rb
@@ -6,11 +6,6 @@ DATABASE = Database.new("../data")
 UF = UpvoteFaker.new
 
 UF.makeTables()
-
-puts Time.at(UF.tsFrom(1,5,"m"))
-puts Time.at(UF.tsFrom(60,60.017,"m"))
-
-UF.makeTables()
 UF.fakeUsers(30)
 UF.fakePosts(100)
 UF.fakeLikes(20)
