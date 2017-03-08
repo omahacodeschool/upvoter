e288f84a6bbdf7d14e325f423cb186807ef5aab8
Evin Grost <evin.grost@gmail.com>
2017-03-08 09:22:57 -0600

    Struggling with function call.

--- public/scripts.js
+++ public/scripts.js
@@ -1,8 +1,15 @@
 window.addEventListener('load', function(){
+
 	var modalBG = document.getElementsByClassName("modalBackground")[0];
 	var modal = document.getElementsByClassName("modal")[0];
 	var newPostButton = document.getElementById("newPostButton");
 
+	for (i = 0; i < 25; i++){
+		var upvoteButton = document.getElementsByClassName("upvoteButton")[i];
+		//HOW TO CALL THIS FUNCTION ON CLICK???
+		upvoteButton.addEventListener('click', function(e){ upvotePost(i) });
+	}
+	
 	newPostButton.addEventListener('click', showModal);
 	modalBG.addEventListener('click', hideModal);
 
@@ -16,5 +23,21 @@ window.addEventListener('load', function(){
 		e.preventDefault();
 		modalBG.style.display = "none";
 		modal.style.display = "none";
+	}
+
+	function upvotePost(event, i) {
+		event.preventDefault();
+		var formClass = document.getElementsByClassName("upvoteform")[i];
+		var testClasses = formClass.className;
+		
+		if (testClasses === "upvoteform"){
+			formClass.className += " articleLiked";
+			event.preventDefault();
+		}
+		else {
+			formClass.className = "upvoteform";
+			event.preventDefault();
+		}
+		
 	}
 });
