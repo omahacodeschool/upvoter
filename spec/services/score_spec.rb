61fef4cd6c67392e8700eb96fb2a8fdc2217066d
Aaron Steward <aa.daly.stew@gmail.com>
2017-03-08 15:15:59 -0600

    all score tests pass

--- spec/services/score_spec.rb
+++ spec/services/score_spec.rb
@@ -25,76 +25,58 @@ RSpec.describe(Score, ".num_likes") do
 	end
 end
 
-# RSpec.describe(Score, ".popular_score") do
-# 	it "calculates score decayed by time" do
+RSpec.describe(Score, ".popular_score") do
+	it "calculates score decayed by time" do
 
-# 		# Setup
-# 		score = Database.new("upvoter_test")
-# 		uf = UpvoteFaker.new
-# 		testNow = uf.tsFrom(0,1,"h")
-# 		testHour = uf.tsFrom(1,7,"h")
-# 		testDay = uf.tsFrom(9,20,"h")
-# 		testWeek = uf.tsFrom(2,5,"d")
-# 		testMonth = uf.tsFrom(2,3,"w")
-# 		testYear = uf.tsFrom(8,40,"w")
-# 		uf.fakePost(testNow)
-# 		uf.fakeLike(testNow, 100)
-# 		uf.fakePost(testHour)
-# 		uf.fakeLike(testHour, 100)
-# 		uf.fakePost(testDay)
-# 		uf.fakeLike(testDay, 100)
-# 		uf.fakePost(testWeek)
-# 		uf.fakeLike(testWeek, 100)
-# 		uf.fakePost(testMonth)
-# 		uf.fakeLike(testMonth, 100)
-# 		uf.fakePost(testYear)
-# 		uf.fakeLike(testYear, 100)
-# 		nowPost = Post.new(testNow)
-# 		hourPost = Post.new(testHour)
-# 		dayPost = Post.new(testDay)
-# 		weekPost = Post.new(testWeek)
-# 		monthPost = Post.new(testMonth)
-# 		yearPost = Post.new(testYear)
+		# Setup
+		score = Database.new("upvoter_test")
+		uf = UpvoteFaker.new
+		testNow = uf.tsFrom(0,1,"h").to_s
+		testHour = uf.tsFrom(1,7,"h").to_s
+		testDay = uf.tsFrom(9,20,"h").to_s
+		testWeek = uf.tsFrom(2,5,"d").to_s
+		testMonth = uf.tsFrom(2,3,"w").to_s
+		testYear = uf.tsFrom(8,40,"w").to_s
+		uf.fakePost(testNow)
+		uf.fakeLike(testNow, 100)
+		uf.fakePost(testHour)
+		uf.fakeLike(testHour, 100)
+		uf.fakePost(testDay)
+		uf.fakeLike(testDay, 100)
+		uf.fakePost(testWeek)
+		uf.fakeLike(testWeek, 100)
+		uf.fakePost(testMonth)
+		uf.fakeLike(testMonth, 100)
+		uf.fakePost(testYear)
+		uf.fakeLike(testYear, 100)
+		nowPost = Post.new(testNow)
+		hourPost = Post.new(testHour)
+		dayPost = Post.new(testDay)
+		weekPost = Post.new(testWeek)
+		monthPost = Post.new(testMonth)
+		yearPost = Post.new(testYear)
 
 
-# 		# Exercise
-# 		scores = [nowPost.score.popular_score, hourPost.score.popular_score, dayPost.score.popular_score, weekPost.score.popular_score, monthPost.score.popular_score, yearPost.score.popular_score]
+		# Exercise
+		scores = [nowPost.score.popular_score, hourPost.score.popular_score, dayPost.score.popular_score, weekPost.score.popular_score, monthPost.score.popular_score, yearPost.score.popular_score]
 		
 
-# 		# Validation
-# 		expect(scores).to be eq([100, 95, 75, 50, 25, 15])
+		# Validation
+		expect(scores).to eq([100, 95.0, 75.0, 50.0, 25.0, 15.0])
 
-# 		# Teardown
-# 		DATABASE.delete("posts", "postid", testNow)
-# 		DATABASE.delete("posts", "postid", testHour)
-# 		DATABASE.delete("posts", "postid", testDay)
-# 		DATABASE.delete("posts", "postid", testWeek)
-# 		DATABASE.delete("posts", "postid", testMonth)
-# 		DATABASE.delete("posts", "postid", testYear)
-# 		DATABASE.delete("likes", "postid", testNow)
-# 		DATABASE.delete("likes", "postid", testHour)
-# 		DATABASE.delete("likes", "postid", testDay)
-# 		DATABASE.delete("likes", "postid", testWeek)
-# 		DATABASE.delete("likes", "postid", testMonth)
-# 		DATABASE.delete("likes", "postid", testYear)
+		# Teardown
+		DATABASE.delete("posts", "postid", "'#{testNow}'")
+		DATABASE.delete("posts", "postid", "'#{testHour}'")
+		DATABASE.delete("posts", "postid", "'#{testDay}'")
+		DATABASE.delete("posts", "postid", "'#{testWeek}'")
+		DATABASE.delete("posts", "postid", "'#{testMonth}'")
+		DATABASE.delete("posts", "postid", "'#{testYear}'")
+		DATABASE.delete("likes", "postid", "'#{testNow}'")
+		DATABASE.delete("likes", "postid", "'#{testHour}'")
+		DATABASE.delete("likes", "postid", "'#{testDay}'")
+		DATABASE.delete("likes", "postid", "'#{testWeek}'")
+		DATABASE.delete("likes", "postid", "'#{testMonth}'")
+		DATABASE.delete("likes", "postid", "'#{testYear}'")
 
-# 	end
-# end
-
-# RSpec.describe(Score, ".decay") do
-# 	it "deprecates a score over time" do
-
-# 		# Setup
-# 		....
-
-# 		# Exercise
-# 		testpost.decay
-
-# 		# Validation
-# 		....
-
-# 		# Teardown
-# 		....
-
-# 	end
-# end
+	end
+end
