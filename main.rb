15532deb3775b64293f215bc1e354b8e23f42163
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-08 10:25:54 -0600

    Require all gems in Gemfile

--- main.rb
+++ main.rb
@@ -1,14 +1,13 @@
-require 'pry'
-
-require 'pg'
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default)
 
 require_relative './services/database.rb'
-DATABASE = Database.new("upvoter_test")
+DATABASE = Database.new("upvoter_development")
 
 require_relative './models/post.rb'
 require_relative './models/user.rb'
 require_relative './services/score.rb'
-require 'sinatra'
 
 enable :sessions
 
