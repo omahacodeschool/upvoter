4504c14f5d25103fb00c422dea424d345089561b
Aaron Steward <aa.daly.stew@gmail.com>
2017-03-09 09:02:30 -0600

    post tests start

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,76 +1,86 @@
 require 'pry'
 
+# Tests whether a user has liked a post
 RSpec.describe(Post, ".likedBy?") do
 	it "checks if a post was liked by a user" do
 
 		# Setup
+		tableCleaner
 
 		# Exercise
 
 		# Verify
 
 		# Teardown
-
+		tableCleaner
 
 	end
 end
 
+# Tests adding and/or removing a like from a post
 RSpec.describe(Post, ".likeClicked") do
 	it "adds or removes a like from a post" do
 
 		# Setup
+		tableCleaner
 
 		# Exercise
 
 		# Verify
 
 		# Teardown
-
+		tableCleaner
 
 	end
 end
 
+# Tests building a new post entry
 RSpec.describe(Post, ".create") do
 	it "creates a new post entry" do
 
 		# Setup
+		tableCleaner
 
 		# Exercise
 
 		# Verify
 
 		# Teardown
-
+		tableCleaner
 
 	end
 end
 
+# Tests returning all entries from posts table
 RSpec.describe(Post, ".all") do
 	it "gathers all post entries" do
 
 		# Setup
+		tableCleaner
 
 		# Exercise
 
 		# Verify
 
 		# Teardown
-
+		tableCleaner
 
 	end
 end
 
+# Tests sorting the entries by different methods
 RSpec.describe(Post, ".sort") do
 	it "arranges post entries" do
 
 		# Setup
+		tableCleaner
 
 		# Exercise
 
 		# Verify
 
 		# Teardown
-
+		tableCleaner
 
 	end
 end
